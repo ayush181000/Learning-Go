79a2c401bdbdc7f99325b7e7dbeb03f3bb143b84
Ayush Garg <ayush181000@gmail.com>
2025-03-10 23:47:23 +0530

    start learning about channels

---
+++ generics.go
@@ -0,0 +1,113 @@
+package main
+
+import "fmt"
+
+// Index returns the index of the first occurrence of elem in slice, or -1 if elem is not present in slice.
+func SlicesIndex[S ~[]E, E comparable](s S, v E) int {
+    for i := range s {
+        if v == s[i] {
+            return i
+        }
+    }
+    return -1
+}
+
+type List[T any] struct {
+    head, tail *element[T]
+}
+
+type element[T any] struct {
+    next *element[T]
+    val  T
+}
+
+func (lst *List[T]) Push(v T) {
+    if lst.tail == nil {
+        lst.head = &element[T]{val: v}
+        lst.tail = lst.head
+    } else {
+        lst.tail.next = &element[T]{val: v}
+        lst.tail = lst.tail.next
+    }
+}
+
+func (lst *List[T]) AllElements() []T {
+    var elems []T
+    for e := lst.head; e != nil; e = e.next {
+        elems = append(elems, e.val)
+    }
+    return elems
+}
+
+func main() {
+    var s = []string{"foo", "bar", "zoo"}
+
+    fmt.Println("index of zoo:", SlicesIndex(s, "zoo"))
+
+    _ = SlicesIndex[[]string, string](s, "zoo")
+
+    lst := List[int]{}
+    lst.Push(10)
+    lst.Push(13)
+    lst.Push(23)
+    fmt.Println("list:", lst.AllElements())
+}[S ~[]E, E comparable](slice S, elem E) int {
+	for i, v := range slice {
+		if v == elem {
+			return i
+		}
+	}
+	return -1
+}
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+// Push adds the element v at the end of the list.
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+// AllElements returns all elements of the list.
+//
+// The returned slice is a copy of the elements; changes to the slice will not affect the list.
+func (lst *List[T]) AllElements() []T {
+	var elems []T
+	for e := lst.head; e != nil; e = e.next {
+		elems = append(elems, e.val)
+	}
+	return elems
+}
+
+// This example shows how to use type parameters to write generic functions.
+//
+// The first example shows how to use the generic Index function to find the index of a string in a slice of strings.
+//
+// The second example shows how to use the type parameter to explicitly specify the type of the slice and element.
+//
+// The third example shows how to use the generic List type to create a list of integers and to get all elements of the list.
+func main() {
+	var s = []string{"foo", "bar", "zoo"}
+
+	fmt.Println("index of zoo:", SlicesIndex(s, "zoo"))
+
+	_ = SlicesIndex[[]string, string](s, "zoo")
+
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+	fmt.Println("list:", lst.AllElements())
+}
